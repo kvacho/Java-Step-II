68d6d46ab0264b98f35c15431b777325474f057f
kvacho <kvachadzegiorgi10@outlook.com>
2025-09-30 11:58:01 +0400

    commit

---
+++ src/Third/Streams.java
@@ -0,0 +1,11 @@
+package Third;
+
+import java.util.Scanner;
+
+public class Streams {
+    public static void run() {
+        // 3
+        Scanner input = new Scanner(System.in);
+        int n;
+    }
+}
